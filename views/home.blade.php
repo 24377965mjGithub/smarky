8ec68c08c9903772958ca31a6a330905f465f814
Mark Jason Espelita <mark.medivadigital@gmail.com>
2024-06-27 05:51:25 +0800

    file upload test logo

--- views/home.blade.php
+++ views/home.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Hello</h1>
-
-    <form action="_file-upload.php" enctype="multipart/form-data" method="post">
-        <input type="file" name="file" id=""> <br>
-        <button type="submit">Upload</button>
-    </form>
+    <h1>Welcome to Smarky App!</h1>
 
 @endsection
